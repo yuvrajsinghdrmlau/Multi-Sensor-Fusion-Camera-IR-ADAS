5920335dfa0051ce5b318f5e814e960eac3d3976
Yuvraj singh <146419722+yuvrajsinghdrmlau@users.noreply.github.com>
2026-03-04 00:51:59 +0530

    Update backbone.py

--- backbone.py
+++ backbone.py
@@ -4,7 +4,7 @@ import torchvision.models as models
 class Backbone(nn.Module):
     def __init__(self):
         super().__init__()
-        model = models.resnet18(pretrained=True)
+        model =  models.resnet18(pretrained=True)
         self.features = nn.Sequential(*list(model.children())[:-2])
 
     def forward(self, x):
